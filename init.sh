6a3f24046b63d3b47b64ca91f0120dbb0d4fb99f
Semennikov Konstantin <Systemko@yandex.ru>
2017-12-13 20:51:24 +0000

    Fix init.sh file

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo etc/init.d/nginx restart
+sudo ln -s -f /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo rm /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
 sudo git config --global user.name "Semennikov Konstantin"
 sudo git config --global user.email Systemko@yandex.ru
