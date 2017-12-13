fc6458d82867178375c176c7d6bfea12aab04bb4
Semennikov Konstantin <systemko@yandex.ru>
2017-12-13 23:37:21 +0300

    Adjust ngingx.conf and init.sh

---
+++ init.sh
@@ -0,0 +1,4 @@
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo etc/init.d/nginx restart
+sudo git config --global user.name "Semennikov Konstantin"
+sudo git config --global user.email Systemko@yandex.ru
