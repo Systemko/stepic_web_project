65bbd2c78f1a6a4a0069a7b41d3c279e4a99ff3f
Semennikov Konstantin <systemko@yandex.ru>
2018-01-20 21:51:49 +0000

    Set gunicorn config file in init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 sudo ln -s -f /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo ln -s -f /home/box/web/etc/hello.py /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
 sudo git config --global user.name "Semennikov Konstantin"
 sudo git config --global user.email Systemko@yandex.ru
