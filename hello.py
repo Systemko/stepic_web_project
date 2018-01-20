06172779175c80908ae0f3b1cc2c45c8e7fdb876
Semennikov Konstantin <systemko@yandex.ru>
2018-01-20 13:44:16 +0000

    Fix test WSGI application hello.py

--- hello.py
+++ hello.py
@@ -1,15 +1,15 @@
-from urllib.parse import parse_qsl
+﻿from urllib.parse import parse_qsl
 
 def wsgi_application(environ, start_response):
     data = ''
     if environ['REQUEST_METHOD'] == 'GET':
         if environ['QUERY_STRING'] != '':
             params = parse_qsl(environ['QUERY_STRING'])
-            for key, value in params.items():
-               data += key + '=' + value + '\n'
+            for item in params:
+               data += item[0] + '=' + item[1] + '\n'
     headers = [
         ('Content-Type','text/plain'),
         ('Content-Length', str(len(data)))
     ]
     start_response('200 OK', headers)
-    return [data]
+    return [data.encode('utf-8')]
