29b2b5ede198cb62c6188f7703a77a041c80b263
Semennikov Konstantin <systemko@yandex.ru>
2018-02-04 13:50:31 +0000

    Adjust init script init.sh for launching gunicorn with ask proiject

--- init.sh
+++ init.sh
@@ -3,5 +3,6 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s -f /home/box/web/etc/hello.py /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
+sudo gunicorn -c /home/box/web/etc/ask.py ask.wsgi:application
 sudo git config --global user.name "Semennikov Konstantin"
 sudo git config --global user.email Systemko@yandex.ru
